fa1a444c1af942af9b2d6eb0c14eaaf3d9cae417
Javier Alfaro <javi.alfaro94@gmail.com>
2025-08-14 04:58:14 +0100

    Add enum for multiple cities

---
+++ scripts/cities.py
@@ -0,0 +1,15 @@
+# cities.py
+from enum import Enum
+
+
+class CityGroups(Enum):
+    INNER_MELBOURNE = [
+        "City of Melbourne, Victoria",
+        "Yarra, Victoria",
+        "City of Port Phillip, Victoria"
+    ]
+    # More groups...
+
+    @classmethod
+    def get(cls, name):
+        return getattr(cls, name).value
