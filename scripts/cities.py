01d661eaf0abf14e0f72ce9c8931dcb19d230f76
Javier Alfaro <javi.alfaro94@gmail.com>
2025-08-20 03:22:44 +0100

    Refactor StateGroups and CityGroups enums

--- scripts/cities.py
+++ scripts/cities.py
@@ -13,3 +13,30 @@ class CityGroups(Enum):
     @classmethod
     def get(cls, name):
         return getattr(cls, name).value
+
+
+class StateGroups(Enum):
+    NSW = ["New South Wales"]
+    VIC = ["Victoria"]
+    QLD = ["Queensland"]
+    SA = ["South Australia"]
+    WA = ["Western Australia"]
+    TAS = ["Tasmania"]
+    NT = ["Northern Territory"]
+    ACT = ["Australian Capital Territory"]
+
+    # Special "all states" option
+    ALL_STATES = [
+        "New South Wales",
+        "Victoria",
+        "Queensland",
+        "South Australia",
+        "Western Australia",
+        "Tasmania",
+        "Northern Territory",
+        "Australian Capital Territory",
+    ]
+
+    @classmethod
+    def get(cls, name):
+        return getattr(cls, name).value
